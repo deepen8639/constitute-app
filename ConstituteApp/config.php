027d3723ec8638f9d939ab687edf58a1727acb86
HondaMasahiro <deepen.8639@ezweb.ne.jp>
2019-09-28 17:43:37 +0900

    config edit

--- ConstituteApp/config.php
+++ ConstituteApp/config.php
@@ -2,10 +2,10 @@
 
 ini_set('display_errors', 1);
 
-define('DB_DATABASE','Constitute');
-define('DB_USERNAME','User');
-define('DB_PASSWORD','Massan24H');
-define('PDO_DSN','mysql:host = localhost;dbname=' . DB_DATABASE);
+define('DB_DATABASE','heroku_7ed077737e560cd');
+define('DB_USERNAME','b0fc4d494cb47b');
+define('DB_PASSWORD','3fe785f3');
+define('PDO_DSN',' mysql://b0fc4d494cb47b:3fe785f3@us-cdbr-iron-east-02.cleardb.net/' . DB_DATABASE);
 
 session_start();
 
