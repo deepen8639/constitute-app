ec0c8e700b6e158a80087656c744e470565aed9a
HondaMasahiro <deepen.8639@ezweb.ne.jp>
2019-10-05 22:11:04 +0900

    config.php added

--- config/config.php
+++ config/config.php
@@ -1,25 +1,27 @@
 <?php
-//ローカル環境用のconfigファイル
-session_start();
+//本番環境用のconfigファイル
 
 ini_set('display_errors', 1);
-// $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-// $db['dbname'] = ltrim($db['path'], '/');
-// $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-// $user = $db['user'];
-// $password = $db['pass'];
-define('DB_DATABASE','Constitute');
-define('DB_USERNAME','User');
-define('DB_PASSWORD','Massan24H');
-define('PDO_DSN','mysql:dbname=Constitute;host=localhost;charset=utf8mb4');
+//データベースの設定
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$user = $db['user'];
+$password = $db['pass'];
+define('DB_DATABASE','heroku_7ed077737e560cd');
+define('DB_USERNAME',$user);
+define('DB_PASSWORD',$password);
+define('PDO_DSN',$dsn);
+
 //urlの設定
-if (empty($_SERVER["HTTPS"])) {
-  $url =  "http://";
-} else {
+if (isset($_SERVER["HTTPS"])) {
   $url =  "https://";
+} else {
+  $url =  "http://";
 }
 
 define('SITE_URL', $url . $_SERVER['HTTP_HOST']);
 
+session_start();
 
  ?>
